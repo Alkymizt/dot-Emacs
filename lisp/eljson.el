db6830eb59e0cb5461922b92d2d1c75e01f5c361
Erik Almara <erik@alkymizt.com>
2019-07-25 12:49:29 -0700

    daily/weekly updates

--- lisp/eljson.el
+++ lisp/eljson.el
@@ -6,37 +6,41 @@
 ;; function eljson - Complexity Analysis:
 ;;    TO DO: Need to figure out how to evaluate ...sn...
 ;;
-; ;...............................................................................
-(defun eljson (json)
-  "Parse JSON and determine if it is of proper JSON format, eventually create
-lisp data structure analoguous to JSON, ELJSON"
-  ;; Need to impliment Hash Tables to make this function work effectively.
-  (let* (
-         (eljson '(("baseObj" . "nil")))
-         (key)
-         (brackets '(("{" . "}")
-                     ("[" . "]")
-                     ("(" . ")")))
-
-         )
-
-    ;;    (cdr (assoc key brackets))
-   (setq eljson (assoc-add "newObj" "null" eljson))
-
-    eljson)
-  )
+;;...............................................................................
 
 ;; "association list" or "alist" for short, records a mapping from keys
 ;; to values. It is a list of cons cells call "associations": the CAR of
 ;; eac cons cell is the "key", and the CDR is the "associated value".
 ;;
-
-(defun assoc-add (key value alist)
+(defun add-to-alist (key value alist)
   "Created dotted-pair from KEY and VALUE and add to ALIST"
-  (let* ()
-    (setq alist (cons '(key . value) alist))
-    alist)
+  (let ()
+    (push (cons key value) alist)
+    )
+  alist)
+
+
+(defun eljson (json)
+  "Parse JSON and determine if it is of proper JSON format, eventually create
+lisp data structure analoguous to JSON, ELJSON"
+  ;; Need to impliment Hash Tables to make this function work effectively.
+  (let* (
+         ;; Make Hash table
+         ;;(eljson (make-hash-table :test 'equal))
+         (eljson '(("jsonObj" . "null")))
+         (key)
+         (brackets '(("{" . "}")
+                     ("[" . "]")
+                     ("(" . ")")))
+         ;;(eljson (alist-add "obj" "{}" eljson))
+         )
+    (setq eljson (add-to-alist "obj" "{}" eljson))
+    ;;    (cdr (assoc key brackets))
+
+    eljson)
   )
 
+
+;;...............................................................................
 
 (eljson "")
