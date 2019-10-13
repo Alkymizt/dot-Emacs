85aa6994b1e48cc41d678a0bb88807b13e3d4030
Erik Almara <erik@alkymizt.com>
2019-10-13 06:12:28 -0700

    clean up

---
+++ elpa/git-commit-20190928.1746/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190928.1746" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "f01338b76587b697af34fd6b5d53d16990899ac9" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
