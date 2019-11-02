60827ad0a85094390b0f16a22b1541fc3338963e
Erik Almara <erik@alkymizt.com>
2019-11-01 21:19:29 -0700

    adding new director/files to repo

--- lisp/parser.el
+++ lisp/parser.el
@@ -1,46 +1,16 @@
-;; parser.el  --- Basic String/JSON parser / generator -*- lexical-binding: t -*-
+;; parser.el  --- Basic String/JSON parser/generator -*- lexical-binding: t -*-
 ;;
 ;; Author: Erik P Almaraz 2019.07.01
 ;;
 ;;
+
+
+
+
+;;
 ;; function isValid - Complexity Analysis:
 ;;    TO DO: Need to figure out how to evaluate ...sn...
 ;;
-;;...............................................................................
-
-
-;; "association list" or "alist" for short, records a mapping from keys
-;; to values. It is a list of cons cells call "associations": the CAR of
-;; eac cons cell is the "key", and the CDR is the "associated value".
-;;
-(defun add-to-alist (key value alist)
-  "Created dotted-pair from KEY and VALUE and add to ALIST"
-  (let ()
-    (push (cons key value) alist)
-    )
-  alist)
-
-(defun ejson (json)
-  "Parse JSON and determine if it is of proper JSON format, eventually create
-lisp data structure analoguous to JSON, ELJSON"
-  ;; Need to impliment Hash Tables to make this function work effectively.
-  (let* (
-         ;; Make Hash table
-         ;;(eljson (make-hash-table :test 'equal))
-         (eljson '(("jsonObj" . "null")))
-         (key)
-         (brackets '(("{" . "}")
-                     ("[" . "]")
-                     ("(" . ")")))
-         ;;(eljson (alist-add "obj" "{}" eljson))
-         )
-    (setq eljson (add-to-alist "obj" "{}" eljson))
-    ;;    (cdr (assoc key brackets))
-
-    eljson)
-  )
-
-;;; Code:
 (defun is-valid (bstring)
   "Parse BSTRING for brackets and determine if they are properly closed"
   (interactive)
@@ -99,3 +69,38 @@ lisp data structure analoguous to JSON, ELJSON"
 (is-valid " ")             ;"Empty String"
 (is-valid "()[]{}[{(}])")  ;"false"
 ;;-----------------------------------------
+
+;;
+;; EXPLORATORY PROGRAMMING
+;;
+
+;; "association list" or "alist" for short, records a mapping from keys
+;; to values. It is a list of cons cells call "associations": the CAR of
+;; eac cons cell is the "key", and the CDR is the "associated value".
+;;
+(defun add-to-alist (key value alist)
+  "Created dotted-pair from KEY and VALUE and add to ALIST"
+  (let ()
+    (push (cons key value) alist)
+    )
+  alist)
+
+(defun ejson (json)
+  "Parse JSON and determine if it is of proper JSON format, eventually create
+lisp data structure analoguous to JSON, ELJSON"
+  ;; Need to impliment Hash Tables to make this function work effectively.
+  (let* (
+         ;; Make Hash table
+         ;;(eljson (make-hash-table :test 'equal))
+         (eljson '(("jsonObj" . "null")))
+         (key)
+         (brackets '(("{" . "}")
+                     ("[" . "]")
+                     ("(" . ")")))
+         ;;(eljson (alist-add "obj" "{}" eljson))
+         )
+    (setq eljson (add-to-alist "obj" "{}" eljson))
+    ;;    (cdr (assoc key brackets))
+
+    eljson)
+  )
