87d7ad50be6bb5ddf7ca76f1f1a9b4cf542a6306
Erik P. Almaraz <erik@almarazlabs.com>
2020-07-15 10:45:38 -0700

    dot-emacs directory/file clean-up and reorganization...

---
+++ lisp/my-modeline/deepspace-modeline.el
@@ -0,0 +1,64 @@
+;;; deepspace-modeline.el --- A minimal mode-line inspired by zerodark. -*- lexical-binding: t; -*-
+
+;; Author: Erik P. Almaraz <erik[at]almarazlabs.com>
+;; Homepage:
+;; Keywords:
+;; Package-Version:
+;; Package-Requires: ((emacs "24.4"))
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+;;
+;;
+
+;;; Code:
+
+;;
+;; Variable Declarations
+;;
+
+
+
+;;
+;; Function Prototypes
+;;
+
+
+
+;;
+;; Configuration
+;;
+
+
+
+;;
+;; Helper Functions
+;;
+
+
+
+;;
+;; Update Functions
+;;
+
+
+
+;;
+;; Segments
+;;
+
+
+
+;;
+;; Activation Functions
+;;
+
+
+
+;;
+;; Provide deepspace-modeline
+;;
+
+
+;;; deepspace-modeline.el ends here
