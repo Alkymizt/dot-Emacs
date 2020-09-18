722415862c6f35c75647a4f231e5dbf3f4d21fef
Erik P. Almaraz <erik@almarazlabs.com>
2020-09-18 12:14:01 -0700

    Minor updates/optimizations to initialization and repo handling

--- init.el
+++ init.el
@@ -1,6 +1,27 @@
 ;; init.el  -*- lexical-binding: t -*-
 ;; Created By: Erik P Almaraz 2019.05.10
+;; Emacs Initialization File: Where all the magic begins!
 ;;
+
+;; Avoid garbage collection during startup, since the GC consumes quite a bit of time, easily doubling
+;; (need to verify this) the start-up time. The trick is to increase the memory threshold in order to
+;; prevent it from running during startup.
+(setq gc-cons-threshold most-positive-fixnum ; 2^16 bytes
+      gc-cons-percentage 0.6)
+;; Every file opened and loaded by Emacs will run through this list to check for a proper handler for
+;; the file, but during startup, it won't need any of them.
+(defvar file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; After Emacs startup has been completed, set `gc-cons-threshold' to
+;; 16 MB and reset `gc-cons-percentage' to its original value.
+;; Also reset `file-name-handler-alist'
+(add-hook 'emacs-startup-hook
+          '(lambda ()
+             (setq gc-cons-threshold (* 16 1024 1024)
+                   gc-cons-percentage 0.1
+                   file-name-handler-alist file-name-handler-alist-original)
+             (makunbound 'file-name-handler-alist-original)))
 
 ;;
 ;; Load Init Files/Libraries
